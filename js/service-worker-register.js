5d1f6f6348c32e3a47219af02ee1d4e7fd09e3e7
Jonathan Fedje <jonathannfedje@gmail.com>
2026-02-15 23:20:24 +0100

    optimize serviceworker

--- js/service-worker-register.js
+++ js/service-worker-register.js
@@ -1,7 +1,7 @@
 // Service Worker Registration
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('service-worker.js', { scope: './' })
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
       .then(function(reg) { console.log('ServiceWorker registered:', reg.scope); })
       .catch(function(err) { console.warn('ServiceWorker registration failed:', err); });
   });
